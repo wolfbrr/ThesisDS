7e0005d2976e9f2c3ebad112f517a8c83d26462e
Boris Wolfson <wolf.brr@gmail.com>
2024-05-26 21:40:01 +0200

    adjusting chain scripts and data

--- DILP/test_recursive.py
+++ DILP/test_recursive.py
@@ -16,12 +16,21 @@ import tensorflow as tf
 import numpy as np
 from src.utils import process_file, process_dir, create_table, output_rules, test_rule, train, test
 from src.generate_template import create_templates
-import duckdb
 tf.random.set_seed(1000)
 np.random.seed(1000)
 
+term_x_0 = Term(True, 'X_0')
+term_x_1 = Term(True, 'X_1')
+input_dir = "../examples/chain-fraud/"
+target, p_e, constants, B, P, N = process_dir(input_dir)
 
-get_ipython().run_cell_magic('time', '', 'term_x_0 = Term(True, \'X_0\')\nterm_x_1 = Term(True, \'X_1\')\ninput_dir = "../examples/chain-fraud/"\ninput_table = create_table(con, input_dir)\nprint(f\'ratio of positives %f\' % (100*input_table[\'Target\'].sum()/len(input_table)))\ntarget, p_e, constants, B, P, N = process_dir(input_dir)\n\np_a, rules = create_templates(p_e, target, term_x_0)\nrules[target] = (rules[target][0], Rule_Template(v=1, allow_intensional=True))\n\n\nlanguage_frame = Language_Frame(target, p_e, constants)\n# program_template = Program_Template(p_a, rules, T=10)\nprogram_template = Program_Template(p_a, rules, T=6)\n')
+p_a, rules = create_templates(p_e, target, term_x_0)
+rules[target] = (rules[target][0], Rule_Template(v=1, allow_intensional=True))
+
+
+language_frame = Language_Frame(target, p_e, constants)
+# program_template = Program_Template(p_a, rules, T=10)
+program_template = Program_Template(p_a, rules, T=6)
 
 
 # In[3]:
